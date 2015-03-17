768df76fed35efbc7350a205fef5c986cbdb41ca
Kevin Mulhern <kevinmulhern23@gmail.com>
2015-03-17 11:29:05 +0000

    Project Finished

---
+++ rest_client.rb
@@ -0,0 +1,34 @@
+require 'rvm'
+require 'rest-client'
+
+puts "==================================".center(70)
+puts "        This is a rest client     ".center(70)
+puts "==================================".center(70)
+puts
+printf "Please enter a query for google to search: "
+input = gets.chomp
+
+# formating the search query so it matches the parameters for google
+input = input.split(" ")
+input = input.join("+")
+
+output = RestClient.get "http://www.google.com/#q=#{input}"
+output = output.to_str
+
+puts 
+puts " This is the output:".center(70)
+puts "------------------------".center(70)
+puts
+puts output.center
+puts 
+
+puts "The response code is:"
+puts output.code
+puts
+
+puts "the headers for the search are:"
+puts output.headers
+puts
+
+puts "The cookies for the search are:"
+puts output.cookies
